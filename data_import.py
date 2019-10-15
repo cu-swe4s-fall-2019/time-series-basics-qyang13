1aac38a1a64233142f5b9ff92116ec4024537c48
Qing Yang <qyang.kevin@gmail.com>
2019-10-14 21:56:31 -0600

    working version all tests added

--- data_import.py
+++ data_import.py
@@ -24,21 +24,19 @@ class ImportData:
         # Use a handle to determine how to summarize duplicated values
         # Default set to 0 meaning sum the duplicated
         self._dedup = 0
-        if 'activity' in data_csv or 'bolus' in data_csv or 'meal' in data_csv:
-            self._dedup = 0
-        elif 'hr' in data_csv or 'smbg' in data_csv or 'cgm' in data_csv or \
-            'basal' in data_csv:
+        if 'hr' in data_csv or 'smbg' in data_csv or 'cgm' in data_csv or \
+           'basal' in data_csv:
             self._dedup = 1
 
-        # open file, create a reader from csv.DictReader, and read input times and values
+        # open file, create a reader from csv.DictReader,
+        # and read input times and values
         with open(data_csv, 'r') as fhandle:
             reader = csv.DictReader(fhandle)
             for row in reader:
                 if 'time' not in row.keys() or 'value' not in row.keys():
-                    raise ValueError('Input data needs contain time and values')
+                    raise ValueError('Input data needs contain time, values')
                 if (row['time'] == ''):
                     continue
-
 
                 try:
                     if row['value'] == 'low':
@@ -56,7 +54,6 @@ class ImportData:
 
             fhandle.close()
 
-
     def linear_search_value(self, key_time):
         '''
         Linear search to find key in the self._time vector
@@ -73,23 +70,26 @@ class ImportData:
         else:
             return(out_list)
 
-    def binary_search_value(self,key_time):
+    def binary_search_value(self, key_time):
         pass
         # optional extra credit
         # return list of value(s) associated with key_time
         # if none, return -1 and error message
 
+
 def roundTimeArray(obj, res):
-    # Inputs: obj (ImportData Object) and res (rounding resoultion)
-    # objective:
-    # create a list of datetime entries and associated values
-    # with the times rounded to the nearest rounding resolution (res)
-    # ensure no duplicated times
-    # handle duplicated values for a single timestamp based on instructions in
-    # the assignment
-    # return: iterable zip object of the two lists
-    # note: you can create additional variables to help with this task
-    # which are not returned
+    '''
+    Inputs: obj (ImportData Object) and res (rounding resoultion)
+    objective:
+    create a list of datetime entries and associated values
+    with the times rounded to the nearest rounding resolution (res)
+    ensure no duplicated times
+    handle duplicated values for a single timestamp based on instructions in
+    the assignment
+    return: iterable zip object of the two lists
+    note: you can create additional variables to help with this task
+    which are not returned
+    '''
     time_lst = []
     vals = []
     num_times = len(obj._time)
@@ -125,7 +125,6 @@ def roundTimeArray(obj, res):
     return output
 
 
-
 def printArray(data_list, annotation_list, base_name, key_file):
     # combine and print on the key_file
     key_list = []
@@ -157,45 +156,44 @@ def printArray(data_list, annotation_list, base_name, key_file):
             writer.writerow([time, val] + old)
     return(0)
 
+
 if __name__ == '__main__':
 
-    #adding arguments
-    parser = argparse.ArgumentParser(description= 'A class to import, combine, and print data from a folder.',
-    prog= 'dataImport')
+    # adding arguments
+    parser = argparse.ArgumentParser(description='import time stuff',
+                                     prog='dataImport')
 
-    parser.add_argument('folder_name', type = str, help = 'Name of the folder')
-
-    parser.add_argument('output_file', type=str, help = 'Name of Output file')
-
-    parser.add_argument('sort_key', type = str, help = 'File to sort on')
-
-    parser.add_argument('--number_of_files', type = int,
-    help = "Number of Files", required = False)
+    parser.add_argument('folder_name', type=str, help='Name of the folder')
+    parser.add_argument('output_file', type=str, help='Name of Output file')
+    parser.add_argument('sort_key', type=str, help='File to sort on')
+    parser.add_argument('--number_of_files', type=int,
+                        help="Number of Files", required=False)
 
     args = parser.parse_args()
 
-    #pull all the folders in the file
+    # pull all the folders in the file
     folder_path = args.folder_name
     try:
         # append file list while checking to see if file exists
-        files_lst = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
+        files_lst = [f for f in listdir(folder_path)
+                     if isfile(join(folder_path, f))]
     except FileNotFoundError:
         print('Specified input folder not found.')
         sys.exit(1)
 
-    #import all the files into a list of ImportData objects (in a loop!)
+    # import all the files into a list of ImportData objects (in a loop!)
     data_lst = []
     for files in files_lst:
-        data_lst.append[ImportData(folder_path + files)]
+        data_lst.append(ImportData(folder_path + '/' + files))
 
-    #create two new lists of zip objects
+    # create two new lists of zip objects
     # do this in a loop, where you loop through the data_lst
-    data_5 = [] # a list with time rounded to 5min
-    data_15 = [] # a list with time rounded to 15min
+    data_5 = []  # a list with time rounded to 5min
+    data_15 = []  # a list with time rounded to 15min
     for i in data_lst:
         data_5.append(roundTimeArray(i, 5))
         data_15.append(roundTimeArray(i, 15))
 
-    #print to a csv file
+    # print to a csv file
     printArray(data_5, files_lst, args.output_file+'_5', args.sort_key)
     printArray(data_15, files_lst, args.output_file+'_15', args.sort_key)
